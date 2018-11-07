2939ff6ad0b2c4974302906681d627880f460022
Cedric Gatay <c.gatay@code-troopers.com>
2018-11-07 21:02:20 +0100

    Initial Commit

---
+++ Cake-Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cake-Pattern
+//
+//  Created by Cedric Gatay on 07/11/2018.
+//  Copyright © 2018 Cedric Gatay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Cake-PatternTests/Cake_PatternTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cake_PatternTests.swift
+//  Cake-PatternTests
+//
+//  Created by Cedric Gatay on 07/11/2018.
+//  Copyright © 2018 Cedric Gatay. All rights reserved.
+//
+
+import XCTest
+@testable import Cake_Pattern
+
+class Cake_PatternTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
